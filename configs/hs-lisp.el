795c56031bfee0aaa51148866a6e9ee9d00ea162
sharmaharish <sharmaharish@gmail.com>
2024-12-01 16:34:22 -0800

    Update lisp config

--- configs/hs-lisp.el
+++ configs/hs-lisp.el
@@ -33,3 +33,6 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
+(setq common-lisp-hyperspec-root
+	  (concat "file://" (expand-file-name "~/Projects/clisp/HyperSpec/")))
